190ef7d3966c0138ebb73afad3938becd7f63c45
Risto Vatsar <r.vatsar@gmail.com>
2023-11-12 17:27:42 +0200

    year input field added

--- scripts/app.js
+++ scripts/app.js
@@ -1,31 +1,54 @@
 const container = document.querySelector(".container");
 const movieTitle = document.querySelector(".userInputTitle");
+const movieReleaseYear = document.querySelector(".userInputYear");
 const moviePosterUrl = document.querySelector(".userInputPosterUrl");
 const btn = document.querySelector(".button");
+//to display
 const movieTitleToDisplay = document.querySelector(".favoriteMovieTitle");
+const movieReleaseYearToDisplay = document.querySelector(".movieReleaseYear");
 
+//get from storage
 let titleInStorage = localStorage.getItem("title");
+let yearInStorage = localStorage.getItem("releaseYear");
 let imageUrlInStorage = localStorage.getItem("imageUrl");
 
-if (titleInStorage && imageUrlInStorage) {
+//display
+if (titleInStorage && yearInStorage && imageUrlInStorage) {
   movieTitleToDisplay.textContent = titleInStorage;
+  movieReleaseYearToDisplay.textContent = `(${yearInStorage})`;
   container.style.backgroundImage = `linear-gradient(
       rgba(173, 216, 230, 0.747),
       rgba(169, 169, 169, 0.745)
     ), url('${imageUrlInStorage}')`;
 }
 
+//event listner
 btn.addEventListener("click", () => {
   let movieTitleInput = movieTitle.value;
+  let movieReleaseYearInput = movieReleaseYear.value;
   let posterUrlInput = moviePosterUrl.value;
+
   localStorage.setItem("title", movieTitleInput);
+  localStorage.setItem("releaseYear", movieReleaseYearInput);
   localStorage.setItem("imageUrl", posterUrlInput);
+
   movieTitleToDisplay.textContent = movieTitleInput;
+
   container.style.backgroundImage = `linear-gradient(
       rgba(173, 216, 230, 0.747),
       rgba(169, 169, 169, 0.745)
     ),
     url('${posterUrlInput}')`;
+
   movieTitle.value = "";
+  releaseYear.value = "";
   moviePosterUrl.value = "";
+
+  if (!posterUrlInput) {
+    container.style.backgroundImage = `linear-gradient(
+      rgba(173, 216, 230, 0.747),
+      rgba(169, 169, 169, 0.745)
+    ),
+    url("../images/image1.jpg")`;
+  }
 });
